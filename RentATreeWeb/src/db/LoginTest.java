59402255fe4c31b0b3aedbd1dac77e2a7a1d0e4e
harisonw <87620631+harisonw@users.noreply.github.com>
2021-11-02 12:09:56 +0000

    Update LoginTest.java

--- RentATreeWeb/src/db/LoginTest.java
+++ RentATreeWeb/src/db/LoginTest.java
@@ -14,14 +14,15 @@ class LoginTest {
 		int success = l.loginUser();
 		assertNotEquals(success, 1); // should reject as no user Test999 with password Test999 in database.
 		
-		// add test user Harison with password Pa$$word123
-		DBConnect db;
-		db = new DBConnect();
-		db.connect();
+		// add test user Harison with password Pa$$word123 - DO THIS Manually else test might fail because of user creation not login
+		
+		//DBConnect db;
+		//db = new DBConnect();
+		//db.connect();
 		//String newuserstr = "IF EXISTS (select 0 from userdetailsmaster where username='Harison') BEGIN CALL createNewUser('Harison','harison@Test.com', 'Harison', 'Wright', '07657865456', 'Pa$$word123', @var) END";
-		String newuserstr = "CALL createNewUser('Harison','harison@Test.com', 'Harison', 'Wright', '07657865456', 'Pa$$word123', @var)";
-		ResultSet output = db.runQuery(newuserstr);
-		System.out.println(output);
+		//String newuserstr = "CALL createNewUser('Harison','harison@Test.com', 'Harison', 'Wright', '07657865456', 'Pa$$word123', @var)";
+		//ResultSet output = db.runQuery(newuserstr);
+		//System.out.println(output);
 		
 		Login l2 = new Login("Harison", "Test");
 		int success2 = l2.loginUser();
