347112943165d928b9ba839d57cfd95f3778ff69
harisonw <87620631+harisonw@users.noreply.github.com>
2021-11-04 13:39:10 +0000

    ignore

--- RentATreeWeb/src/db/DBConnect.java
+++ RentATreeWeb/src/db/DBConnect.java
@@ -18,7 +18,7 @@ public class DBConnect {
 	public DBConnect() {
 		conn = null;
 		this.database  = "rentatree";
-		this.username = "root";
+		this.username = "Pa$$word";
 		this.password = "pass";
 		this.url = "jdbc:mysql://localhost:3306/"+this.database+"?useTimezone=true&serverTimezone=GMT";
 		//System.out.println(this.url);
